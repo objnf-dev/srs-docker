f4fd0a68e4cfbf79b29cb98aaf6a16f0a8461fdb
Zhou Weitong <xml@live.com>
2017-12-22 13:00:27 +0800

    122201

--- shell/start_srs.sh
+++ shell/start_srs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /root/shell
-./srs.sh
+nohup ./srs.sh
 nohup ./mse.sh > /root/logs/mse_log/mse_$(date "+%Y-%m-%d-%H-%M-%S").log 2&>1
 while true;
 do sleep 1;
--- shell/stop.sh
+++ shell/stop.sh
@@ -2,7 +2,4 @@
 sudo killall -9 srs
 sudo killall -9 shell
 sudo killall -9 mse
-sudo killall -9 start_go-oryx.sh
-sudo killall -9 start.sh
-sudo killall -9 start_srs.sh
 exit
