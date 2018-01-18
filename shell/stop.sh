134dc80de00e556bd528ec12b03bd3517d5f55d1
Zhou Weitong <xml@live.com>
2018-01-18 13:17:46 +0800

    Signed-off-by: Zhou Weitong <xml@live.com>

--- shell/stop.sh
+++ shell/stop.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+echo "Kill SRS"
 sudo killall -9 srs
+echo "Kill Go-oryx"
 sudo killall -9 shell
+echo "Kill Websocket Proxy"
 sudo killall -9 mse
+echo "Kill HTTP&HTTPS Proxy"
 sudo killall -9 main
 exit
